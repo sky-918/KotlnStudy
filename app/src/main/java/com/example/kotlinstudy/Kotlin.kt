b3394107bd5a83ab9d363ae853e8a8434d4a9504
tyy <yangyang.tian@5imakeup.com>
2020-10-15 09:52:27 +0800

    添加了kotlin的main方法

---
+++ app/src/main/java/com/example/kotlinstudy/Kotlin.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinstudy
+
+/**
+ *
+ * 可以随便运行kotlin语言的一个文件
+ */
+fun main(args: Array<String>) {
+    print("dasdasdasd\n")
+    // list add和set的区别
+    val list = mutableListOf<String>()
+    list.add("1")
+    list.set(0, "2323")
+    list.add(1, "2323")
+    println("list.size=${list.size}")
+}
