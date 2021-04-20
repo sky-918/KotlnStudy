f69371ab332dca401aa9592a486bb75757e28828
tianyangyang <tianyangyang@mysteel.com>
2021-04-20 15:06:43 +0800

    提交

--- app/src/main/java/com/example/kotlinstudy/round/RoundCornerActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/round/RoundCornerActivity.kt
@@ -1,16 +1,20 @@
 package com.example.kotlinstudy.round
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.kotlinstudy.R
 
 class RoundCornerActivity : AppCompatActivity() {
     companion object{
+        @SuppressLint("LogNotTimber")
         fun newInstance(context:Context){
             val intent=Intent(context,RoundCornerActivity::class.java)
             context.startActivity(intent)
+            Log.d( "newInstance: ","测试")
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
