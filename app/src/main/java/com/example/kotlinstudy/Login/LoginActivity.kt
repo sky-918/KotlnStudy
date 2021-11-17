b8593eee17c54c3d985beff0b7d5d5262ab4995c
tianyangyang <1004994051@qq.com>
2021-11-17 17:24:07 +0800

    feat:增加文字对齐自定义view

--- app/src/main/java/com/example/kotlinstudy/Login/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/Login/LoginActivity.kt
@@ -12,6 +12,9 @@ import com.example.kotlinstudy.round.RoundCornerActivity
 import com.example.kotlinstudy.time.CountdownActivity
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import android.view.ViewTreeObserver
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+
 
 class LoginActivity : RxAppCompatActivity(), ILoginContract.ILoginView {
     lateinit var operationString: String
@@ -38,21 +41,42 @@ class LoginActivity : RxAppCompatActivity(), ILoginContract.ILoginView {
         //tyy 绑定view
         loginPreset.attachView()
 
+        Log.d("dsadadas","宽度："+tv_text2.width)
+        //获得ViewTreeObserver
+        //获得ViewTreeObserver
+        val observer: ViewTreeObserver = tv_text2.getViewTreeObserver()
+//注册观察者，监听变化
+        observer.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                //判断ViewTreeObserver 是否alive，如果存活的话移除这个观察者
+//               val obs=tv_text2.viewTreeObserver
+//                obs.removeOnGlobalLayoutListener(this)
+//                    //获得宽高
+//                    val viewWidth: Int = tv_text2.getMeasuredWidth()
+//                    val viewHeight: viewHeightInt = tv_text2.getMeasuredHeight()
+                    Log.d("dsadadas","宽度1111："+tv_text2.width)
+                if (observer.isAlive){
+                    observer.removeOnGlobalLayoutListener(this)
+                }
+
+            }
+        })
     }
 
     private val onClickListener = View.OnClickListener { v ->
         when (v.id) {
             R.id.btn_login -> {
+                Log.d("dsadadas","宽度："+tv_text2.width)
                 val userName: String = et_account.text.toString()
                 val password: String = et_password.text.toString()
                 Log.i(Tag, "这是登录：userName=$userName     password=$password")
 
                 operationString = "登录"
 
-                loginPreset.onLogin(this, userName, password)
-
-
-                CountdownActivity.newInstance(this)
+//                loginPreset.onLogin(this, userName, password)
+//
+//
+//                CountdownActivity.newInstance(this)
             }
             R.id.btn_register -> {
                 val userName: String = et_account.text.toString()
--- app/src/main/java/com/example/kotlinstudy/utils/imageload/ImageLoader.java
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinstudy.utils.imageload;
-
-public interface ImageLoader {
-    void putPic();
-
-    void getPic();
-}
